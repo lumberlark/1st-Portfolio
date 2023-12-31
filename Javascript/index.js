91af8f70ca05b62eaaa3cf89ca236896e75b8e94
lumberlark <slark20@gmail.com>
2023-12-30 20:56:49 -0600

    Update 1.0

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,19 +1,19 @@
 const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
+const navLinks = document.querySelectorAll('.nav__link');
 
-navToggle.addEventListener('click',() => {
-    document.body.classList.toggle('nav-open');
+navToggle.addEventListener('click', () => {
+  document.body.classList.toggle('nav-open');
 });
 
 navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
-    })
-})
-
-document.querySelector('#contact-form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    e.target.elements.name.value = '';
-    e.target.elements.email.value = '';
-    e.target.elements.message.value = '';
+  link.addEventListener('click', () => {
+    document.body.classList.remove('nav-open');
   });
+});
+
+document.querySelector('#contact-form').addEventListener('submit', e => {
+  e.preventDefault();
+  e.target.elements.name.value = '';
+  e.target.elements.email.value = '';
+  e.target.elements.message.value = '';
+});
