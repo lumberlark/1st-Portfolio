9a309f7bf8ad39ed15c0907b46543eb1b4e14346
lumberlark <slark20@gmail.com>
2023-12-30 23:01:34 -0600

    added project files.

--- email_form.php
+++ email_form.php
@@ -1,16 +1,24 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $name = $_POST["name"];
   $email = $_POST["email"];
   $message = $_POST["message"];
 
-  $to = "slark20@gmail.com"; // Replace with your email address
+  $to = "slark20@gmail.com"; 
   $subject = "New Contact Form Submission";
 
   // Set headers
   $headers = "MIME-Version: 1.0" . "\r\n";
   $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
   $headers .= "From: $name <$email>" . "\r\n";
+
+  // Debugging: Output form data
+  echo "Name: $name<br>";
+  echo "Email: $email<br>";
+  echo "Message: $message<br>";
 
   // Attempt to send the email
   if (mail($to, $subject, $message, $headers)) {
