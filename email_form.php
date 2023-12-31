c0992e1b71837a1e8ee8f71c5eb0418fcf32b741
lumberlark <slark20@gmail.com>
2023-12-30 22:19:21 -0600

    Hotfix-email update.

--- email_form.php
+++ email_form.php
@@ -6,8 +6,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
   $to = "slark20@gmail.com"; // Replace with your email address
   $subject = "New Contact Form Submission";
-  $headers = "From: $name <$email>";
 
-  mail($to, $subject, $message, $headers);
+  // Set headers
+  $headers = "MIME-Version: 1.0" . "\r\n";
+  $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+  $headers .= "From: $name <$email>" . "\r\n";
+
+  // Attempt to send the email
+  if (mail($to, $subject, $message, $headers)) {
+    echo "Email sent successfully!";
+  } else {
+    echo "Failed to send email. Please try again later.";
+  }
 }
 ?>
