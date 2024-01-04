0c260414d21592b0150d451f6c3783d5415e62ca
lumberlark <slark20@gmail.com>
2024-01-04 06:39:08 -0600

    updated contact form JS for server response

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,5 +1,6 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
+const submitBtn = document.getElementById('sendmessage');
 
 navToggle.addEventListener('click', () => {
   document.body.classList.toggle('nav-open');
@@ -11,9 +12,13 @@ navLinks.forEach(link => {
   });
 });
 
-document.querySelector('#contact-form').addEventListener('submit', e => {
+document.querySelector('.contact-form').addEventListener('submit', e => {
   e.preventDefault();
   e.target.elements.name.value = '';
   e.target.elements.email.value = '';
   e.target.elements.message.value = '';
+});
+
+submitBtn.addEventListener('click', () => {
+  alert('Email service is Temporary down, Apologies for the inconvenience');
 });
